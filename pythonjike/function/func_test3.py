06f1863bcff51e0f708bd5d2e8625f5039626ff4
huanbinwu <huanbin2580@163.com>
2019-01-25 22:05:23 +0800

    $

---
+++ pythonjike/function/func_test3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# Author:HuanBing
+
+#闭包的使用
+#a*x+b=Y
+def a_line(a,b):
+    def arg_y(x):
+        return a*x+b
+    return arg_y
+#a=3 b=5
+#x=10 y=?
+
+line1=a_line(3,5)
+line2=a_line(5,10)
+print(line1(10))
+print(line1(20))
+
