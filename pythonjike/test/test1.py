89344fc921518503580c0b3144c754cf6fe5096f
huanbinwu <huanbin2580@163.com>
2019-01-25 22:31:58 +0800

    这是我的第一个python程序

---
+++ pythonjike/test/test1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# Author:HuanBing
+#这是我的第一个Python程序
+import time # 我导入了一个时间模块
+print(time.time())#在屏幕上打印到1970年1月1日到现在过了多少秒了。
+if 10-9 > 0 :
+    #缩进
+    print("10大于9")
