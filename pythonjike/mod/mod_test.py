db28ed5e4175d3d30ea1131096b52d6ae343e848
huanbinwu <huanbin2580@163.com>
2019-01-25 22:16:27 +0800

    模块的定义

---
+++ pythonjike/mod/mod_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# Author:HuanBing
+
+#模块的定义
+
+# import os
+# import  time
+# time.sleep()
+
+# import matplotlib as m
+# # # from matplotlip
+# from time import  sleep
+# sleep()
+
+
+#自定义模块
+import  mymod
+mymod.print_me()
